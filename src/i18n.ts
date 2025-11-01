724b50b9a957e426263567f868f698c840db812b
claralmoura <claramoura2303@gmail.com>
2025-10-31 22:14:38 -0300

    fix: ajusta as traduções para prod

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,18 @@
 import { createI18n } from 'vue-i18n'
-import pt from './locales/pt.json'
-import en from './locales/en.json'
+
+const messages = Object.fromEntries(
+  Object.entries(
+    import.meta.glob<Record<string, any>>('./locales/*.json', { eager: true })
+  ).map(([key, value]) => {
+    return [key.slice(10, -5), value.default] as const
+  })
+)
 
 const i18n = createI18n({
   legacy: false,
   locale: 'pt',
   fallbackLocale: 'en',
-  messages: {
-    pt,
-    en
-  }
+  messages,
 })
 
 export default i18n
